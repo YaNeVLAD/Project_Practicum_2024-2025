546f8fe2477fd997d5034c0531e72ee1d07f4198
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2024-10-03 20:43:07 +0300

    initial commit

--- My_Game/Project_Practicum/main.cpp
+++ My_Game/Project_Practicum/main.cpp
@@ -2,8 +2,7 @@
 #include "GameConstants.hpp"
 #include "Player.h"
 #include "lab2.h"
-#include "lab3.h"
-
+	 
 using namespace sf;
 
 void setWindowSettings(RenderWindow& window);
@@ -14,7 +13,7 @@ int main()
 	RenderWindow window(VideoMode::getDesktopMode(), "My Game");
 	setWindowSettings(window);
 
-	Event event{};
+	Event event;
 
 	Player player(window.getSize().x / 2, window.getSize().y / 2);
 	player.initShape();
@@ -22,101 +21,6 @@ int main()
 	//std::vector<GameObject> state;
 	//state.insert(state.end(), player);
 
-	//LAB2.1
-	RectangleShape background = RectangleShape(Vector2f(150, 330));
-	background.setFillColor(Color::White);
-	background.setPosition(150, 250);
-
-	CircleShape greenCircle = CircleShape(50);
-	greenCircle.setFillColor(Color::Green);
-	greenCircle.setPosition(175, 260);
-
-	CircleShape yellowCircle = CircleShape(50);
-	yellowCircle.setFillColor(Color::Yellow);
-	yellowCircle.setPosition(175, 365);
-
-	CircleShape redCircle = CircleShape(50);
-	redCircle.setFillColor(Color::Red);
-	redCircle.setPosition(175, 470);
-
-	//LAB2.2
-	RectangleShape KLeft = RectangleShape(Vector2f(50, 200));
-	KLeft.setFillColor(Color::Blue);
-	KLeft.setPosition(400, 200);
-
-	RectangleShape KTop = RectangleShape(Vector2f(50, 150));
-	KTop.setFillColor(Color::Blue);
-	KTop.setRotation(-135);
-	KTop.setPosition(450, 300);
-
-	RectangleShape KBottom = RectangleShape(Vector2f(50, 150));
-	KBottom.setFillColor(Color::Blue);
-	KBottom.setRotation(-55);
-	KBottom.setPosition(430, 320);
-
-
-	RectangleShape VLeft1 = RectangleShape(Vector2f(50, 200));
-	VLeft1.setFillColor(Color::Blue);
-	VLeft1.setRotation(-45);
-	VLeft1.setPosition(600, 250);
-
-	RectangleShape VRight1 = RectangleShape(Vector2f(50, 200));
-	VRight1.setFillColor(Color::Blue);
-	VRight1.setRotation(45);
-	VRight1.setPosition(850, 218);
-
-
-	RectangleShape VLeft2 = RectangleShape(Vector2f(50, 200));
-	VLeft2.setFillColor(Color::Blue);
-	VLeft2.setRotation(-45);
-	VLeft2.setPosition(600 + 300, 250);
-
-	RectangleShape VRight2 = RectangleShape(Vector2f(50, 200));
-	VRight2.setFillColor(Color::Blue);
-	VRight2.setRotation(45);
-	VRight2.setPosition(850 + 300, 218);
-
-	//LAB2.3
-	ConvexShape roof;
-	roof.setFillColor(Color::Red);
-	roof.setPosition(1200, 600);
-	roof.setPointCount(4);
-	roof.setPoint(0, { -90, 0 });
-	roof.setPoint(1, { +90, 0 });
-	roof.setPoint(2, { +220, 90 });
-	roof.setPoint(3, { -220, 90 });
-
-	RectangleShape walls = RectangleShape(Vector2f(350, 300));
-	walls.setFillColor(Color(0x44, 0x21, 0x12));
-	walls.setPosition(1020, 690);
-
-	RectangleShape door = RectangleShape(Vector2f(100, 150));
-	door.setFillColor(Color::Black);
-	door.setPosition(1050, 850);
-
-	RectangleShape pipe1 = RectangleShape(Vector2f(50, 80));
-	pipe1.setFillColor(Color(0x80, 0x80, 0x80));
-	pipe1.setPosition(1230, 570);
-
-	RectangleShape pipe2 = RectangleShape(Vector2f(80, 40));
-	pipe2.setFillColor(Color(0x80, 0x80, 0x80));
-	pipe2.setPosition(1215, 550);
-
-	CircleShape smoke1 = CircleShape(20);
-	smoke1.setFillColor(Color(0xA0,0xA0,0xA0));
-	smoke1.setPosition(1250, 520);
-
-	CircleShape smoke2 = CircleShape(25);
-	smoke2.setFillColor(Color(0xA0, 0xA0, 0xA0));
-	smoke2.setPosition(1265, 495);
-
-	CircleShape smoke3 = CircleShape(30);
-	smoke3.setFillColor(Color(0xA0, 0xA0, 0xA0));
-	smoke3.setPosition(1275, 460);
-
-	CircleShape smoke4 = CircleShape(35);
-	smoke4.setFillColor(Color(0xA0, 0xA0, 0xA0));
-	smoke4.setPosition(1275, 420);
 
 	while (window.isOpen())
 	{
@@ -148,37 +52,10 @@ int main()
 		//state.push_back(player);
 		//
 		//Draw objects
-
-		//for (int i = 0; i < trafficLights.size(); i++)
-		//{
-		//	window.draw(trafficLights[i]);
-		//}
-
-		window.draw(background);
-		window.draw(greenCircle);
-		window.draw(yellowCircle);
-		window.draw(redCircle);
-
-
-		window.draw(KLeft);
-		window.draw(KTop);
-		window.draw(KBottom);
-		window.draw(VLeft1);
-		window.draw(VRight1);
-		window.draw(VLeft2);
-		window.draw(VRight2);
-
-
-		window.draw(roof);
-		window.draw(walls);
-		window.draw(door);
-		window.draw(smoke1);
-		window.draw(smoke2);
-		window.draw(smoke3);
-		window.draw(smoke4);
-		window.draw(pipe1);
-		window.draw(pipe2);
-
+		/*for (int i = 0; i < state.size(); i++)
+		{
+			i.draw(window);
+		}*/
 
 		player.draw(window);
 
@@ -212,3 +89,4 @@ void handleInput(Player& player, Event::KeyEvent event)
 //Логлайн - краткое описание сюжета и положения дел в игре
 //Таблица сравнения - название игры, что в ней понравилось, насколько понравилось
 //MindMap - возможные состояния игры, например, меню/геймплей, у меню есть несколько пунктов и т.д.
+//!НЕРЕАЛЬНО! Нарисовать круг
