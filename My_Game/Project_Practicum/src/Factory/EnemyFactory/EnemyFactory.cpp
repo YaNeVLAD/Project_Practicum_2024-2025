92cb8c7c88c72402372d470220f6ad8c40d1df43
Ковалев Владислав <vlad.kovalev1503@gmail.com>
2024-11-15 23:23:26 +0300

    Добавил спрайты врагам

--- My_Game/Project_Practicum/src/Factory/EnemyFactory/EnemyFactory.cpp
+++ My_Game/Project_Practicum/src/Factory/EnemyFactory/EnemyFactory.cpp
@@ -1,15 +1,19 @@
 #include "EnemyFactory.h"
 
 #include "../../Manager/Entity/EntityManager.h"
+#include "../../../utils/SpriteSheet.h"
 
 void EnemyFactory::Create(EntityManager& entityManager, float x, float y)
 {
 	auto& enemy = entityManager.CreateEntity(EntityType::Enemy);
 	enemy.AddComponent<TransformComponent>(x, y, 0.0f, 0.0f);
-	enemy.AddComponent<DrawableComponent>(40, 40, sf::Color::Red);
 
-	auto collisionShape = std::make_unique<sf::RectangleShape>(sf::Vector2f(40, 40));
-	collisionShape->setOrigin(20.0f, 20.0f);
-	enemy.AddComponent<CollisionComponent>(std::move(collisionShape));
+	auto collisionShape = std::make_unique<sf::RectangleShape>(sf::Vector2f(50, 50));
+	collisionShape->setOrigin(25, 25);
+	enemy.AddComponent<CollisionComponent>(std::move(collisionShape), sf::Vector2f(0, 15));
 	enemy.AddComponent<HealthComponent>(50, 0.5f);
+
+	std::vector<sf::Texture> frames = SpriteSheet::LoadTextures("assets/melee_enemy/Walk.png", 128, 128);
+	enemy.AddComponent<AnimationComponent>(frames, 0.2f, true, -1.0f, true);
+	enemy.AddComponent<DrawableComponent>(frames[0], sf::Vector2f(0.75f, 0.75f));
 }
